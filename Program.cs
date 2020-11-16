2a3ad3912752fe70c21e18298ec4ab25110e945f
KellyCristinaAires <k.woys@hotmail.com>
2020-11-15 22:21:59 -0300

    atividade mundo azul

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace mundoazul
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Quem faz seu dia feliz? ");
+            Console.Write("  Permita se ser feliz!! ");
+            Console.ReadKey();
+        
+            Console.WriteLine(" Não deixe que ninguém ofusque seu brilho ");
+            Console.WriteLine(" Você é especial :) ");
+            Console.WriteLine(" Deixe seu mundo ser Azul :)");
+        }
+    }
+}
